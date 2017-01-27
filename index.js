a01beb76592cd74b76aca156b86b9015230e2906
wextia <valcazar96@gmail.com>
2017-01-27 22:24:47 +0100

    login screen made

--- index.js
+++ index.js
@@ -14,7 +14,38 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (socket){
+var users = {};
+// Number of connected users at the moment
+var numUsers = 0;
 
+io.on('connection', function (socket){
+  var addedUser = false;
+
+  socket.on('add user', function (username) {
+    if(addedUser)
+      return;
+
+    if(!users[username]){
+      // Adding new user to our database
+      var user = {};
+      user.score = 0;
+      user.username = username;
+      users[username] = user;
+    }
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,3 +1,62 @@
 $(function() {
-  var socket = io();
+  var $window = $(window);
+  // Input field for the username
+  var $usernameInput = $('.usernameInput');
+
+  var $loginPage = $('.login.page');
+  var $mainPage = $('.main.page');
+
+  var username;
+  var connected=false;
+  var $currentInput = $usernameInput.focus();
+
+  var socket=io();
+
+  // Sets the client's username
+  function setUsername () {
+    username = cleanInput($usernameInput.val().trim());
+
+    // If the username is valid
+    if (username) {
+      $loginPage.fadeOut();
+      $mainPage.show();
+      $loginPage.off('click');
+      // Tell the server your username
+      socket.emit('add user', username);
+    }
+  }
+
+  // Prevents input from having injected markup
+  function cleanInput (input) {
+    return $('<div/>').text(input).text();
+  }
+
+
+  $window.keydown(function (event) {
+    // Auto-focus the current input when a key is typed
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+      $currentInput.focus();
+    }
+    // When the client hits ENTER on their keyboard
+    if (event.which === 13) {
+      if (username) {
+        sendMessage();
+        socket.emit('stop typing');
+        typing = false;
+      } else {
+        setUsername();
+      }
+    }
+  });
+
+  // Focus input when clicking anywhere on login page
+  $loginPage.click(function () {
+    $currentInput.focus();
+  });
+
+  socket.on('login', function(data){
+    connected = true;
+    console.log("You are connected.")
+  });
+
 });
