5bea0b027183205cc373f9204c508865ab9cc07c
wextia <valcazar96@gmail.com>
2017-01-29 19:30:39 +0100

    added buttons, auto button update, etc

--- index.js
+++ index.js
@@ -15,8 +15,52 @@ server.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 
 var users = {};
+// Users connected at the moment
+var onlineUsers={};
 // Number of connected users at the moment
 var numUsers = 0;
+
+function print_users(){
+  console.log('Printing all users...')
+  for (u in users){
+    console.log('  ' + users[u].username);
+  }
+}
+
+function save_users(){
+
+}
+
+function load_users(){
+
+}
+
+function errorUserNotExists(username) {
+  console.log('User ' + username + ' does not exist.');
+}
+
+function errorUserNotConnected(username) {
+  console.log('User ' + username + ' is not connected.');
+}
+
+function errorDuringTransaction(data){
+  var su = data[0];
+  var ru = data[1];
+  console.log('Cannot complete transaction between ' +
+    su + ' and ' + ru + '. Reason:');
+}
+
+function logUserScore(username){
+  console.log('User ' + username + ' has ' +
+  users[username].score + ' points.');
+}
+
+function logTransactionCompleted(data){
+  var su = data[0];
+  var ru = data[1];
+  console.log('Transaction between ' +
+    su + ' and ' + ru + ' completed successfully.');
+}
 
 io.on('connection', function (socket){
   var addedUser = false;
@@ -28,24 +72,86 @@ io.on('connection', function (socket){
     if(!users[username]){
       // Adding new user to our database
       var user = {};
-      user.score = 0;
+      user.score = 10;
       user.username = username;
       users[username] = user;
+      console.log('New user added: ' + username);
+      print_users();
     }
 
-    // we store the username in the socket session for this client
+    // We add the user to our connected users list to enable direct communication
+    // between users in real time
+    onlineUsers[username] = socket;
+    // We store the username in the socket session for this client
     socket.username = username;
-    ++numUsers;
     addedUser = true;
+
     socket.emit('login', {
-      numUsers: numUsers
+      score: users[username].score,
+      users: users
     });
 
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
+    socket.broadcast.emit('added user', {
+      score: users[username].score,
+      users: users
     });
+
   });
-});
 
+// Data contains the names of the users sending(0) and recieving(1) the
+// score
+  socket.on('add score', function(data){
+    var sender_username = data[0];
+    var reciever_username = data[1];
+
+    var sender = users[sender_username]
+    if(!sender){
+      errorDuringTransaction(data);
+      errorUserNotExists(sender_username);
+      return;
+    }
+
+    if(sender.score <= 0){
+      errorDuringTransaction(data);
+      logUserScore(sender_username);
+      return;
+    }
+
+    var reciever = users[reciever_username];
+    if(!reciever){
+      errorDuringTransaction(data);
+      errorUserNotExists(reciever_username);
+      return;
+    }
+
+    reciever.score += 1;
+    sender.score -= 1;
+
+    // We check if the sender is connected right now
+    // if she's not, we log an error, because this
+    // action cannot (presumably) be done offline
+    // FIXME maybe i should bail out of the transaction
+    // if the sender is not online
+    var sender_socket = onlineUsers[sender_socket];
+
+    if(sender_socket){
+      sender_socket.emit('update score', sender.score);
+    }
+    else {
+      errorUserNotConnected(sender_socket);
+    }
+
+    var reciever_socket = onlineUsers[reciever_username];
+
+    if(reciever_socket){
+      reciever_socket.emit('update score', reciever.score);
+    }
+
+    // We log all the delicious messages of victory
+    logTransactionCompleted(data);
+    logUserScore(sender_username);
+    logUserScore(reciever_username);
+
+  });
+
+});
--- public/main.js
+++ public/main.js
@@ -4,9 +4,16 @@ $(function() {
   var $usernameInput = $('.usernameInput');
 
   var $loginPage = $('.login.page');
+  // Main page, where the score is displayed and the main interaction with
+  // the app happens
   var $mainPage = $('.main.page');
+  // Here we hold a reference to a ul from which hang all buttons
+  // representing the registered users.
+  var $userButtons = $('.user.button.list')
+  // Label with the user score
+  var $userScore = $('.user.score');
 
-  var username;
+  var current_username;
   var connected=false;
   var $currentInput = $usernameInput.focus();
 
@@ -14,16 +21,41 @@ $(function() {
 
   // Sets the client's username
   function setUsername () {
-    username = cleanInput($usernameInput.val().trim());
+    current_username = cleanInput($usernameInput.val().trim());
 
     // If the username is valid
-    if (username) {
+    if (current_username) {
       $loginPage.fadeOut();
       $mainPage.show();
       $loginPage.off('click');
       // Tell the server your username
-      socket.emit('add user', username);
+      socket.emit('add user', current_username);
     }
+  }
+
+  // Erases all button inside the ul
+  function eraseUserButtons(){
+    $('.user.button.list button').remove();
+  }
+
+  // This creates all the buttons hanging from the user buttons list
+  function createUsersButtons(users){
+    for(u in users){
+      var rec_username = users[u].username;
+      if(rec_username == current_username)
+        continue;
+
+      var $butt = $('<button/>', {
+        text: rec_username,
+        id: u,
+        click: function () {addScore(this)}
+      });
+      $userButtons.append($butt);
+    }
+  }
+
+  function addScore(elem){
+    socket.emit("add score", [current_username, elem.id])
   }
 
   // Prevents input from having injected markup
@@ -39,7 +71,7 @@ $(function() {
     }
     // When the client hits ENTER on their keyboard
     if (event.which === 13) {
-      if (username) {
+      if (current_username) {
         sendMessage();
         socket.emit('stop typing');
         typing = false;
@@ -54,9 +86,16 @@ $(function() {
     $currentInput.focus();
   });
 
-  socket.on('login', function(data){
-    connected = true;
-    console.log("You are connected.")
+  socket.on('added user', function(data){
+    // We update the users buttons
+    eraseUserButtons();
+    createUsersButtons(data.users);
   });
 
+  socket.on('login', function(data){
+    connected = true;
+    console.log("You are connected.");
+    $userScore.text('Score: ' + data.score);
+    createUsersButtons(data.users);
+  });
 });
